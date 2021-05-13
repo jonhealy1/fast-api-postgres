b25d8efa29b440162d06b3f54f1e4284f310b638
jonhealy1 <jonathan.d.healy@gmail.com>
2021-05-13 13:55:51 -0700

    add pre-commit

--- src/app/api/crud.py
+++ src/app/api/crud.py
@@ -1,16 +1,23 @@
 from app.api.models import NoteSchema
-from app.db import notes, database
-import json
+from app.db import database, notes
+
 
 async def post(payload: NoteSchema):
     data_payload = payload.data
     # data_payload = json.loads(r'{"data": {"test": 1, "hello": "I have \" !"}, "id": 4}')
-    query = notes.insert().values(title=payload.title, description=payload.description, geometry=payload.geometry, data=data_payload)
+    query = notes.insert().values(
+        title=payload.title,
+        description=payload.description,
+        geometry=payload.geometry,
+        data=data_payload,
+    )
     return await database.execute(query=query)
+
 
 async def get(id: int):
     query = notes.select().where(id == notes.c.id)
     return await database.fetch_one(query=query)
+
 
 async def get_all():
     # query = notes.select()
@@ -23,15 +30,20 @@ async def get_all():
     # query = notes.select().where(notes.c.geometry.ST_Intersects('LINESTRING(2 1,4 1)'))
     return await database.fetch_all(query=query)
 
+
 async def put(id: int, payload: NoteSchema):
     query = (
-        notes
-        .update()
+        notes.update()
         .where(id == notes.c.id)
-        .values(title=payload.title, description=payload.description, geometry=payload.geometry)
+        .values(
+            title=payload.title,
+            description=payload.description,
+            geometry=payload.geometry,
+        )
         .returning(notes.c.id)
     )
     return await database.execute(query=query)
+
 
 async def delete(id: int):
     query = notes.delete().where(id == notes.c.id)
